721ff5ca2c94ced65fb97af8e09f25e1aec3894c
Eril <136311206+ErilW@users.noreply.github.com>
2025-11-17 00:26:42 +0700

    changing all

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
         size=HYPERPARAMS["input_shape"][:2]
     )
     output_root="/root/augmented_balanced_dataset4"
-    path = create_yolo_balanced_dataset(input_root=datasets_segmentation, output_root=output_root, )
+    path = create_yolo_balanced_dataset(input_root=datasets_segmentation, output_root=output_root, ratio=25 )
     model = YOLO("yolov8s-cls.pt")
 
     data = model.train(
