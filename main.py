2772f02558bf48c135d7a93253858ea799c7dd48
eril <eril.sanjaya@satnusa.com>
2025-11-19 08:02:12 +0700

    update main

--- main.py
+++ main.py
@@ -30,26 +30,26 @@ def preprocessing(ham_path, segmentations_path, output_dir="./preprocessed_datas
 
 
 def main():
-    # path_ham10000 = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
-    # path_segmentations = kagglehub.dataset_download("tschandl/ham10000-lesion-segmentations")
-    # path_segmentations = f"{path_segmentations}/HAM10000_segmentations_lesion_tschandl"
-    #
-    # datasets_output = "./root/preprocessed_datasets5"
-    # datasets_segmentation = "./root/segmentation_masks5"
-    # output_experiments = f"experiments_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
-    # os.makedirs(output_experiments, exist_ok=True)
+    path_ham10000 = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
+    path_segmentations = kagglehub.dataset_download("tschandl/ham10000-lesion-segmentations")
+    path_segmentations = f"{path_segmentations}/HAM10000_segmentations_lesion_tschandl"
 
-    # preprocessing(
-    #     ham_path=path_ham10000,
-    #     segmentations_path=path_segmentations,
-    #     output_dir=datasets_output,
-    #     output_segmentations_dir=datasets_segmentation,
-    #     size=HYPERPARAMS["input_shape"][:2]
-    # )
+    datasets_output = "./root/preprocessed_datasets5"
+    datasets_segmentation = "./root/segmentation_masks5"
+    output_experiments = f"experiments_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+    os.makedirs(output_experiments, exist_ok=True)
+
+    preprocessing(
+        ham_path=path_ham10000,
+        segmentations_path=path_segmentations,
+        output_dir=datasets_output,
+        output_segmentations_dir=datasets_segmentation,
+        size=None
+    )
     output_root="./root/augmented_balanced_dataset5"
-    model = YOLO(r"yolo12-cls.pt")
+    model = YOLO(r"yolo11x-cls.pt")
 
-    # path = create_yolo_balanced_dataset(input_root=datasets_segmentation, output_root=output_root, ratio=2 )
+    path = create_yolo_balanced_dataset(input_root=datasets_segmentation, output_root=output_root, ratio=2 )
 
     # mini summary
     # train 14: balance: 0.5, full aug:0.72
@@ -62,7 +62,7 @@ def main():
         patience=20,
         epochs=100,
         batch=32,
-        imgsz=224,
+        imgsz=640,
         lr0=0.01,
         momentum=0.9,
         weight_decay=0.0001,
